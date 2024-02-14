d77b5748f32d92bf3a01243c246a5f3ac966dbef
Shashank Thakur <shashankt2004@gmail.com>
2024-02-14 23:45:48 +0530

    dynamic README generation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 
+README_FILE = "README.md"
+
 
 def get_current_xkcd():
     url = "https://xkcd.com/info.0.json"
@@ -8,17 +10,28 @@ def get_current_xkcd():
     return data
 
 
-def print_comic_details(comic_data):
-    print("Title:", comic_data['title'])
-    print("Comic Number:", comic_data['num'])
-    print("Date:", f"{comic_data['month']}/{comic_data['day']}/{comic_data['year']}")
-    print("Image URL:", comic_data['img'])
-    print("Alt Text:", comic_data['alt'])
+def generate_readme(comic_data):
+    readme_content = f"""
+        # Current XKCD Comic
+        
+        Title: {comic_data['title']}  
+        Comic Number: {comic_data['num']}  
+        Date: {comic_data['month']}/{comic_data['day']}/{comic_data['year']}  
+        
+        ---
+        
+        ![XKCD Comic]( {comic_data['img']} )  
+        
+        {comic_data['alt']}
+    """
+    with open(README_FILE, "w") as readme_file:
+        readme_file.write(readme_content)
 
 
 def main():
     current_comic = get_current_xkcd()
-    print_comic_details(current_comic)
+    generate_readme(current_comic)
+    print(f"README.md updated with the current XKCD comic ({current_comic['num']})")
 
 
 if __name__ == "__main__":
