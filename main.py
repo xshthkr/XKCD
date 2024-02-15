2eba840676509b9289548282b06dc1f25c1eb64d
Shashank Thakur <shashankt2004@gmail.com>
2024-02-15 14:54:12 +0530

    add README.md update status badge

--- main.py
+++ main.py
@@ -16,8 +16,11 @@ def generate_readme(comic_data):
     readme_content = f"""
 <h1 align="center"><a href="https://xkcd.com">XKCD</a></h1>
 <div align="center">
-    <a href="https://github.com/ShashashankThakur/XKCD/actions/workflows/main.yml"><img src="https://github.com/ShashashankThakur/XKCD/actions/workflows/main.yml/badge.svg?event=status"/></a>
+    <a href="https://github.com/ShashashankThakur/XKCD/actions/workflows/main.yml">
+        <img src="https://github.com/ShashashankThakur/XKCD/actions/workflows/main.yml/badge.svg" alt="Update XKCD Comic">
+    </a>
 </div>
+
 <p align="center"><i>Dynamically updated and generated every midnight UTC</i></p>
 <hr>
 <div align="center">
